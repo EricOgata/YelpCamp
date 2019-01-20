f3f9f1465cb35244b28bba054b42184572f63a46
Eric Ogata <ericogata@ymail.com>
2019-01-20 15:01:21 -0200

    Modificações para Heroku

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var commentRoutes 	 = require("./routes/comments"),
 	campgroundRoutes = require("./routes/campgrounds"),
 	indexRoutes		 = require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
+// mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
