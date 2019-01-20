b68b3a87962d8f09c78a319bf5d6548ad7905c48
Eric Ogata <ericogata@ymail.com>
2019-01-20 15:06:08 -0200

    app.js minor fix

--- app.js
+++ app.js
@@ -47,6 +47,6 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/", indexRoutes);
 
-app.listen(3000, function(req, res){
+app.listen(process.env.PORT, process.env.IP, function(req, res){
 	console.log("YelpCamp App Has Started");
 });
